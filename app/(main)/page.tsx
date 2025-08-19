7b8e87322c2d7de13324629c28cea1d381cd9746
Emo Onerhime <emo.onerhime@gmail.com>
2025-08-19 13:27:22 +0100

    Install Next.js, Tailwind CSS and create the initial template

---
+++ app/(main)/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Homepage() {
+  return (
+    <div className="flex justify-center items-center  h-screen text-2xl text-red-700">
+      Homepage
+    </div>
+  );
+}
